c745211e0091523de7f55362e990575ca03b297e
Gustavo Rojas <gustavo.rojas@ellipticmarketing.com>
2021-12-18 00:45:20 -0500

    Fix baseUrl for production

--- config.production.php
+++ config.production.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'https://rojasgustavo.com/',
+    'baseUrl' => 'https://rojasgustavo.com',
     'isIndexSelected' => function ($page) {
         return $page->getUrl() == 'https://rojasgustavo.com/' ? 'text-white border-white' : 'text-gray-500 border-transparent hover:text-gray-300';
     },
